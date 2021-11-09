78b5e2810713d35bf1d852e12ef35fad61cb3232
p22cgussen <p22cgussen@fairfieldprep.org>
2021-11-09 10:18:35 -0500

    completehw6-2

---
+++ hw6-2.py
@@ -0,0 +1,32 @@
+# Author CCG 11/9/2021
+
+# Question 1
+variable = list([0,1,2,3,4,5,6,7])
+
+# Question 2
+print(len(variable))
+
+# Question 3
+print(variable[2])
+
+# Question 4
+print(5 not in variable)
+
+# Question 5
+print(10 not in variable)
+
+# Question 6
+variable2 = list(["charlie" , "declan" , "michael" , "nolan"])
+
+# Question 7
+print(variable2[0])
+
+# Question 8
+print(variable2 * 3)
+
+# Question 9
+new_lst = variable + variable2
+
+# Question 10
+print(len(new_lst))
+
